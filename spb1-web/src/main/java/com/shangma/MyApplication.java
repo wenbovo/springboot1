5843c07e61c96dcfd64d54afeeaee5a413b91c7a
boger <bogerxue@foxmail.com>
2022-07-13 19:51:02 +0800

    Initial commit

---
+++ spb1-web/src/main/java/com/shangma/MyApplication.java
@@ -0,0 +1,18 @@
+package com.shangma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.tags.HtmlEscapeTag;
+
+/**
+ * @author BOGEY
+ * 2022/7/12  22:35
+ */
+@SpringBootApplication
+public class MyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class,args);
+
+
+    }
+}
---
+++ spb1-web/src/main/java/com/shangma/controller/Test1.java
@@ -0,0 +1,9 @@
+package com.shangma.controller;
+
+/**
+ * @author BOGEY
+ * 2022/7/12  22:53
+ */
+
+public class Test1 {
+}
