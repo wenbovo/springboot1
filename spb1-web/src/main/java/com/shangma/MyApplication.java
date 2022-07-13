748f3e6949fca2e1825438dd124e6cf482111c68
boger <bogerxue@foxmail.com>
2022-07-13 20:39:56 +0800

    "关于yml的使用"

--- spb1-web/src/main/java/com/shangma/MyApplication.java
+++ spb1-web/src/main/java/com/shangma/MyApplication.java
@@ -2,7 +2,6 @@ package com.shangma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.tags.HtmlEscapeTag;
 
 /**
  * @author BOGEY
@@ -12,7 +11,5 @@ import org.springframework.web.servlet.tags.HtmlEscapeTag;
 public class MyApplication {
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class,args);
-
-
     }
 }
--- spb1-web/src/main/java/com/shangma/controller/Test1.java
+++ spb1-web/src/main/java/com/shangma/controller/Test1.java
@@ -1,9 +1,21 @@
 package com.shangma.controller;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author BOGEY
  * 2022/7/12  22:53
  */
 
+@Component
+@RestController
 public class Test1 {
+
+    @GetMapping("test")
+    public String test1(){
+        return "success";
+    }
+
 }
